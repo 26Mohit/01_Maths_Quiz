58c7ca559136559bc45044a3962c52763245c850
26Mohit <60586130+26Mohit@users.noreply.github.com>
2020-06-08 19:46:18 +1200

    Update 03_questions_generation_v1.py

--- 03_questions_generation_v1.py
+++ 03_questions_generation_v1.py
@@ -1,21 +1,20 @@
 import random
 
 # Generate number of questions that user has entered
-selected_questions = int(input("please enter number of questions:"))
-
 # Generate numbers according to the values entered by user
-lowest = int(input("please enter a low number"))
-highest = int(input("please enter a high number"))
-range = lowest-highest
-num_1 = random.randint(lowest, highest)
-num_2 = random.randint(lowest, highest)
+selected_questions = int(input("please enter number of questions:"))
+for selected_questions in range(selected_questions):
+    lowest = int(input("please enter a low number"))
+    highest = int(input("please enter a high number"))
+    range = lowest-highest
+    num_1 = random.randint(lowest, highest)
+    num_2 = random.randint(lowest, highest)
+    question = "{} x {} = ".format(num_1, num_2)
+    correct = num_1 * num_2
+    answer = int(input(question))
 
-question = "{} x {} = ".format(num_1, num_2)
-correct = num_1 * num_2
-answer = int(input(question))
-
-if answer == correct:
-  print("Great job")
-else:
-  print("oops")
+    if answer == correct:
+      print("Great job")
+    else:
+      print("oops")
 
