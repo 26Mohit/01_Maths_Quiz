3cc953fa1b5d3ae73117546f6bc893e1925ec9d0
26Mohit <60586130+26Mohit@users.noreply.github.com>
2020-06-09 10:55:23 +1200

    Commit

--- 03_questions_generation_v1.py
+++ 03_questions_generation_v1.py
@@ -2,6 +2,7 @@ import random
 
 # Generate number of questions that user has entered
 # Generate numbers according to the values entered by user
+# Generate questions according to the level user chose
 selected_questions = int(input("please enter number of questions:"))
 for selected_questions in range(selected_questions):
     lowest = int(input("please enter a low number"))
@@ -9,8 +10,8 @@ for selected_questions in range(selected_questions):
     range = lowest-highest
     num_1 = random.randint(lowest, highest)
     num_2 = random.randint(lowest, highest)
-    question = "{} x {} = ".format(num_1, num_2)
-    correct = num_1 * num_2
+    question = "{} - {} = ".format(num_1, num_2)
+    correct = num_1 - num_2
     answer = int(input(question))
 
     if answer == correct:
